db4f1f91f1ab9b416e0090b7ac82f55e7c7b1148
musta <mustafaenfadal@gmail.com>
2020-10-09 16:56:17 +0200

    merge

--- database/database.js
+++ database/database.js
@@ -6,10 +6,6 @@ var connection = mysql.createConnection({
     database: 'ecommerce'
 });
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 6c7a8abd8dc44586f28b86cbd8d0d2bf37eb6a6f
 connection.connect(function(error){
     if(error){
        throw error;
